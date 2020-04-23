b6e046c5aabfa6f8945ad654b3da98ff4e1e3869
mp3 <im.in.a.coma.xxx@gmail.com>
2020-04-23 17:47:56 +0900

    set video from s3

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom'
 
 const Container = () => {
   return (
-    <div>{'Hello, world'}</div>
+    <video
+      src="https://study-in-video-element.s3-ap-northeast-1.amazonaws.com/IMG_2415_1080.mov"
+      controls={true}
+      width="100%"
+    >
+        not supported
+      </video>
   )
 }
 
