07b1107ebcd9ab07a903e5b02f9a9184b902bf74
mp3 <im.in.a.coma.xxx@gmail.com>
2020-04-23 16:22:36 +0900

    barebone react app

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,10 @@
-console.log('hello')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Container = () => {
+  return (
+    <div>{'Hello, world'}</div>
+  )
+}
+
+ReactDOM.render(<Container />, document.getElementById('container'))
